5b5ab8a7e70e578bac7be9581d00395b701b7ec4
Mikolenn <64177564+Mikolenn@users.noreply.github.com>
2021-07-16 22:30:48 -0600

    First commit
    Programas para el servidor y el cliente en UPD

---
+++ ClienteUDP.py
@@ -0,0 +1,32 @@
+import socket
+
+#socket de IPv4 y UDP
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+continuar = True
+while continuar:
+
+    #ingresar caracteres a convertir
+    dataOut = raw_input("[Cliente] Ingrese una cadena de caracteres" + "\n")
+
+    if dataOut != "fin":
+
+        #convertir de string a bytes para enviarlo
+        dataOut = bytes(dataOut.encode("utf-8"))
+
+        #enviarlo
+        s.sendto(dataOut, ("192.168.0.5", 1024))
+
+        #recibir el mensaje
+        dataIn, addr = s.recvfrom(1024)
+
+        #convertirlo de bytes a string
+        dataIn = str(dataIn.decode("utf-8"))
+        print("\n" + "[Cliente] Se recibe: " + dataIn + "\n")
+
+    #STOP termina la concexion
+    else:
+        dataOut = bytes(dataOut.encode("utf-8"))
+        s.sendto(dataOut, ("192.168.0.5", 1024))
+        s.close() 
+        continuar = False
---
+++ ServidorUDP.py
@@ -0,0 +1,34 @@
+import socket
+
+#socket de IPv4 y UDP
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#asociar socket
+s.bind(("192.168.0.5", 1024))
+
+continuar = True
+while continuar:
+
+    #recibir el mensaje
+    dataIn, addr = s.recvfrom(1024)
+
+    #convertirlo de bytes a string
+    dataIn = dataIn.decode("utf-8")
+
+    if dataIn != str("fin"):
+        print("\n" + "[Servidor] Se recibe: " + dataIn)
+
+        #de minusculas a mayusculas
+        dataOut = dataIn.upper()
+
+        #convertir de string a bytes para enviarlo
+        dataOut = bytes(dataOut.encode("utf-8"))
+
+        #enviarlo
+        s.sendto(dataOut, addr)
+
+    #STOP termina la concexion
+    else:
+        s.close()
+        print("\n" + "[Servidor] Fin de la conexion " + str(addr))
+        continuar = False
